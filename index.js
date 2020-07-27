5665a1d69281f15efe843687f41e64c52755e930
Joshua Barnett <joshua@barne.tt>
2020-07-27 14:31:04 +0100

    Fix CORS issue.

--- index.js
+++ index.js
@@ -1,93 +1,87 @@
+// IMPORTANT:
+// if you're using the EU endpoint, use https://eu.api.fpjs.io URL instead.
+const API_URL = 'https://api.fpjs.io';
+const COOKIE_DURATION_DAYS = 365;
+const COOKIE_DURATION_MS = COOKIE_DURATION_DAYS * 24 * 60 * 60 * 1000;
 // This cookie name will be used to store the visitorID
 // cookie will be set by Cloudflare worker in a 1st party context (http-only and secure)
-const CookieKey = "__cflvid";
-
-// A special visitorID that's used in situations when a visitorID 
+const COOKIE_NAME = '__cflvid';
+// A special visitorID that's used in situations when a visitorID
 // should not be generated (throttled requests, search bots, unauthorized requests etc)
-const VisitorIdNa = "n/a";
-
-addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleRequest(event.request))
-  } catch (e) {
-    event.respondWith(`{"visitorId": "${VisitorIdNa}", "reason": "${e.message}"}`)
-  }
-});
-
-async function handleRequest(request) {
-  const remoteClientIP = request.headers.get("CF-Connecting-IP")
-  // IMPORTANT: 
-  // if you're using the EU endpoint, use https://eu.api.fpjs.io URL instead.
-  const apiUrl = "https://api.fpjs.io";
-
-  const headers = {
-    "Connection": "keepalive",
-    "Content-Type": "application/json",
-    // I'm using a non-standard header, 
-    // because CF overwrites standard headers with their IPs
-    "Remote-Client-IP": remoteClientIP,
-    "User-Agent": request.headers.get("User-Agent")
-  };
-
-
-
-  const cookieValue = getRequestCookieValue(request.headers.get("Cookie"), CookieKey);
-  if (cookieValue) {
-    headers["Cfl-Vid"] = cookieValue;
-  }
-
-  const apiRequest = new Request(apiUrl, {
-    method: "POST",
-    body: request.body,
-    headers: headers
-  });
-
-
-  let apiResponse = await fetch(apiRequest);
-
-  let responseJson = await apiResponse.json();
-
-  let responseHeaders = { "Content-Type": "application/json" };
-
-  const responseCookie = generateResponseCookie(responseJson.visitorId);
-  if (responseCookie) {
-    responseHeaders["Set-Cookie"] = responseCookie;
-  }
-
-  return new Response(JSON.stringify(responseJson), {
-    headers: responseHeaders
-  });
-}
-
-function getRequestCookieValue(rawCookieHeader, key) {
-  let value = null;
-  if (!rawCookieHeader || rawCookieHeader === "") {
-    return value;
-  }
-  let pairs = rawCookieHeader.split(/;\s?/);
-  if (pairs.length === 0) {
-    return value;
-  }
-  pairs.forEach(pair => {
-    let [currentKey, currentValue] = pair.split("=");
-
-    if (currentKey === key) {
-      value = currentValue;
-      return;
-    }
-  });
-  return value;
-}
+const VISITOR_ID_NA = 'n/a';
 
 function generateResponseCookie(visitorId) {
-  if (VisitorIdNa === visitorId) {
+  if (!visitorId || VISITOR_ID_NA === visitorId) {
     return;
   }
-  const cookieDurationDays = 365;
-  const nameValue = `${CookieKey}=${visitorId}`;
-  const path = "path=/";
-  var date = new Date();
-  date.setTime(date.getTime() + (cookieDurationDays * 24 * 60 * 60 * 1000));
-  let expires = `expires=${date.toUTCString()}`;
-  return [nameValue, path, expires, "HttpOnly", "Secure"].join("; ");
+  const nameValue = `${COOKIE_NAME}=${visitorId}`;
+  const path = 'path=/';
+  const date = new Date();
+  date.setTime(date.getTime() + COOKIE_DURATION_MS);
+  const expires = `expires=${date.toUTCString()}`;
+  return [nameValue, path, expires, 'HttpOnly', 'Secure'].join('; ');
 }
+
+/**
+ * Grabs the cookie with name from the request headers
+ * @param {Request} request incoming Request
+ * @param {string} name of the cookie to grab
+ * https://developers.cloudflare.com/workers/templates/pages/cookie_extract/
+ */
+function getCookie(request, name) {
+  let result = '';
+  const cookieString = request.headers.get('Cookie');
+  if (cookieString) {
+    const cookies = cookieString.split(';');
+    cookies.forEach(cookie => {
+      const cookieName = cookie.split('=')[0].trim();
+      if (cookieName === name) {
+        const cookieVal = cookie.split('=')[1];
+        result = cookieVal;
+      }
+    })
+  }
+  return result;
+}
+
+async function handleRequest(request) {
+  try {
+    const remoteClientIP = request.headers.get('CF-Connecting-IP');
+    const headers = Object.fromEntries(request.headers.entries());
+    // I'm using a non-standard header,
+    // because CF overwrites standard headers with their IPs
+    headers['Remote-Client-IP'] = remoteClientIP;
+    const requestCookie = getCookie(request, COOKIE_NAME);
+    if (requestCookie) {
+      headers['Cfl-Vid'] = requestCookie;
+    }
+    const apiRequest = new Request(API_URL, {
+      ...request,
+      headers
+    });
+    const apiResponse = await fetch(apiRequest);
+    const response = apiResponse.clone();
+    if (apiResponse.headers.get('Content-Type') === 'application/json') {
+      const responseJson = apiResponse.json();
+      const responseCookie = generateResponseCookie(responseJson.visitorId);
+      if (responseCookie) {
+        response.headers.set('Set-Cookie', responseCookie);
+      }
+    }
+    return response;
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        visitorId: VISITOR_ID_NA,
+        reason: error.message
+      }), {
+        status: 500
+      }
+    );
+  }
+}
+
+addEventListener('fetch', event => {
+  const { request } = event;
+  event.respondWith(handleRequest(request));
+});
